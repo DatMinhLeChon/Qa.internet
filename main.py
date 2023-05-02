5750be2a44971e7ce52f44ac2ee5be7f0eb00db9
Dat <dat.lemindast@gmail.com>
2023-05-02 15:54:32 +0700

    change

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import os
 import pandas as pd
+import json
+
+def running():
+    json_path = 'dev.json'
+
+    json_open = open(json_path)
+    json_load = json.load(json_open)
+
+    print(json_load)
+
+if __name__ == "__main__":
+    running()
