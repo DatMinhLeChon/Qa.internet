6f06cc7f4b0a2de773f6889f9c1de72970b26e57
lechonminhdat <dat.lemindast@gmail.com>
2023-05-03 21:05:05 +0700

    Change

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import json
 from fuzzywuzzy import fuzz
+
 # jsol light:
 # id
 # questions
@@ -13,23 +14,19 @@ class QaPairs():
     def __init__(self, question, answer):
         self.question =question
         self.answer = answer
-class Annotation():
-    def __init__(self, type, content):
-        self.type = type
-        self.content = content
-    
-    def parseAnswer(self):
-        if self.type == "singleAnswer":
-            return self.content
-        elif self.type == "multipleQAs":
-            return 
-        
-        
+        self.score = None
 
-class DataItem():
-    def __init__(self, question, annotation):
+class MultipleAnswer():
+    def __init__(self, this_id, question, qa_pairs):
+        self.id = this_id
         self.question = question
-        self. annotation = annotation
+        self.qa_pairs = qa_pairs
+        
+class SingleAnswer():
+    def __init__(self, id, question, answer):
+        self.id = id
+        self.question = question
+        self.answer = answer
         self.score = None
 
 class DataQA():
@@ -38,22 +35,32 @@ class DataQA():
         self.data_item_list = data_item_list
         self.weight = 0.1
         
-    def databaseVocab(self):
-        
+    def databaseVocab(self, vocab_db):
+        item_list = []
         for item in self.data_item_list:
-            fuzz()
+            for item2 in vocab_db:
+            ratio = fuzz(item, item2 )
         
         return
         
 def parsingDataset():
+    single_container, annotation_container = [] , []
     json_path = 'train_light.json' # dataset
     json_open = open(json_path)
     json_load = json.load(json_open)
     for item in json_load:
         dict = item["annotations"][0] # parse the only 0 index list to this content 
         if dict.get("type") == "singleAnswer":
+            single_container.append(SingleAnswer(item["id"], item["question"], item["annotations"][0].get("answer")))
+        elif dict.get("type") == "multipleQAs":
+            annotation_container_temp = []
+            dict_temp = (dict.get("qaPairs"))[0]
+                #annotation_container_temp.append(QaPairs(item2.get("question"), item2.get("answer")))
+            annotation_container.append(MultipleAnswer(item["id"], item["question"], dict_temp))
+    return single_container, annotation_container
             
-            
+def fuzzyMatching():
+    
 
 if __name__ == "__main__":
     parsingDataset()
